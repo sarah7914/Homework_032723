8aefdc82306c255fde981f1f1efc9d5594f6156a
sarah7914 <128432606+sarah7914@users.noreply.github.com>
2023-03-25 11:29:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 
-df_repair = pd.read_csv('repair_data.csv')
+
 df_sales = pd.read_csv('sales_data.csv')
 
 
@@ -12,6 +12,6 @@ st.set_page_config(layout="wide")
 st.image('https://5ypbvxa39ihl3fage541b0i.blob.core.windows.net/media/Frigidaire_Media/Logo/elements-logos-color.svg', width=250) 
 st.title('2022 Refrigerator Warranty Claim Data')
 
-st.write(df_repair)
+
 
 st.write(df_sales)
